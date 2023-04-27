f0d693d1492a39a63aad81bba0592e8889e7bd2a
Bucur Dan-Alexandru <bucur.alexandru.dan@gmail.com>
2023-04-27 19:08:32 +0300

    Backup old project

--- app/src/main/java/com/bellum/blacker/MainActivity.kt
+++ app/src/main/java/com/bellum/blacker/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.bellum.blacker
 
+import android.content.pm.PackageManager
+import android.content.pm.PackageManager.PERMISSION_DENIED
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -10,7 +14,13 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
+import com.bellum.blacker.main.DisplayMusicAlt
+import com.bellum.blacker.main.DisplayMusics
+import com.bellum.blacker.main.getAllMP3Alt
+import com.bellum.blacker.main.readFromFile
 import com.bellum.blacker.ui.theme.BlackerTheme
+import java.util.jar.Manifest
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,23 +28,42 @@ class MainActivity : ComponentActivity() {
         setContent {
             BlackerTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android") //Init Development
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    //DisplayMusics(getAllMP3Alt(this))
+                    if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) == PERMISSION_DENIED){
+                        shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                        ActivityResultContracts.RequestPermission()
+                    }
+                    /*
+                    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                        requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1000)
+                    }*/
+                    //Text(text = readFromFile("data.txt"))
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    BlackerTheme {
-        Greeting("Android")
+/*
+  class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.Q)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        StrictMode.enableDefaults()
+        setContent {
+            BlackerTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    DisplayMusics(getAllMP3(this)) /*val songList: List<Song> = listOf() DisplaySongs(songList)*/
+                }
+            }
+        }
     }
 }
+*/
